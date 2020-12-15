9273c5209d4d924545924af627a775e7f743b4b9
Luis Fer Jiménez <>
2020-12-15 09:56:10 -0600

    Def

---
+++ 01_Getting_started/HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+12.11.2020
+Objective: Display a message in the console
+*/
+
+public class HelloWorld{
+	
+	// main method
+	public static void main(String[] args){
+		System.out.println("Hello world!");
+	}
+}
